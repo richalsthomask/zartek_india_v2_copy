41ad722e08359710755dc5143b75ecf3cc4731dc
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-06-17 19:59:12 +0530

    fix(purge css): ignore main css purge

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,7 +123,7 @@ module.exports = {
         printRejected: false, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        ignore: ["style.css"], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         purgeCSSOptions: {
           // https://purgecss.com/configuration.html#options
