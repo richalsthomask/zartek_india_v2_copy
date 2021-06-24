4e076e51c34a48a73eb2aa804f24b29e27f36364
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-06-24 15:00:07 +0530

    chore: minified Js and CSS

--- src/components/Partner/index.tsx
+++ src/components/Partner/index.tsx
@@ -1,5 +1,7 @@
-import React, { PureComponent } from "react";
+import { graphql, StaticQuery } from "gatsby";
+import React, { Fragment, PureComponent } from "react";
 import { Layout } from "../Layout";
+import SEO from "../SEO";
 import { Breadcrumb } from "../Shared/Breadcrumb";
 
 export default class PartnerWithUs extends PureComponent {
@@ -13,29 +15,48 @@ export default class PartnerWithUs extends PureComponent {
             { path: "/partner-with-us/", title: "Partner with us" },
           ]}
         />
-        <div
-          className="typeform-widget"
-          data-url="https://tyiilhexsjg.typeform.com/to/LiVxOGBv"
-          data-transparency="100"
-          data-hide-headers="true"
-          data-hide-footer="true"
-          style={{ width: "100%", height: "515px" }}
-        >
-          <div className="widget__WidgetWrapper-sc-1rxjz1n-0 NwqzB">
-            <div className="widget__IframeWrapper-sc-1rxjz1n-2 cKLpKc">
-              <iframe
-                frameBorder="0"
-                height="100%"
-                src="https://tyiilhexsjg.typeform.com/to/LiVxOGBv?typeform-medium=embed-snippet"
-                width="100%"
-                allow="camera; microphone; autoplay; encrypted-media;"
-                data-qa="iframe"
-                title="typeform-embed"
-                style={{ border: "0px", minHeight: "515px" }}
-              ></iframe>
-            </div>
-          </div>
-        </div>
+
+        <StaticQuery
+          query={graphql`
+            query {
+              contentfulSeo(page: { eq: "Partner with us" }) {
+                metaTitle
+                metaDescription
+                metaUrl
+                metaAuthor
+                metaKeywords
+              }
+            }
+          `}
+          render={({ contentfulSeo }) => (
+            <Fragment>
+              <SEO contentfulSeo={contentfulSeo} />
+              <div
+                className="typeform-widget"
+                data-url="https://tyiilhexsjg.typeform.com/to/LiVxOGBv"
+                data-transparency="100"
+                data-hide-headers="true"
+                data-hide-footer="true"
+                style={{ width: "100%", height: "515px" }}
+              >
+                <div className="widget__WidgetWrapper-sc-1rxjz1n-0 NwqzB">
+                  <div className="widget__IframeWrapper-sc-1rxjz1n-2 cKLpKc">
+                    <iframe
+                      frameBorder="0"
+                      height="100%"
+                      src="https://tyiilhexsjg.typeform.com/to/LiVxOGBv?typeform-medium=embed-snippet"
+                      width="100%"
+                      allow="camera; microphone; autoplay; encrypted-media;"
+                      data-qa="iframe"
+                      title="typeform-embed"
+                      style={{ border: "0px", minHeight: "515px" }}
+                    ></iframe>
+                  </div>
+                </div>
+              </div>
+            </Fragment>
+          )}
+        />
       </Layout>
     );
   }
