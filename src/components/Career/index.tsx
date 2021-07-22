87f51e38263a62ae99047e94e118d401d5485702
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-07-22 17:44:32 +0530

    chore(careers): new iframe

--- src/components/Career/index.tsx
+++ src/components/Career/index.tsx
@@ -30,27 +30,16 @@ export default class Career extends Component {
           render={({ contentfulSeo }) => (
             <Fragment>
               <SEO contentfulSeo={contentfulSeo} />
-              <div
-                className="typeform-widget"
-                data-url="https://form.typeform.com/to/dK0O5FbP"
-                data-transparency="100"
-                data-hide-headers="true"
-                data-hide-footer="true"
-                style={{ width: "100%", height: "515px" }}
-              >
-                <div className="widget__WidgetWrapper-sc-1rxjz1n-0 NwqzB">
-                  <div className="widget__IframeWrapper-sc-1rxjz1n-2 cKLpKc">
-                    <iframe
-                      frameBorder="0"
-                      height="100%"
-                      src="https://form.typeform.com/to/dK0O5FbP?typeform-medium=embed-snippet"
-                      width="100%"
-                      allow="camera; microphone; autoplay; encrypted-media;"
-                      data-qa="iframe"
-                      title="typeform-embed"
-                      style={{ border: "0px", minHeight: "515px" }}
-                    ></iframe>
-                  </div>
+              <div className="page-bottom pbottom-70">
+                <div className="container pt-3">
+                  <iframe
+                    src="https://zartek.kekahire.com/api/embedjobs/a8fa4f07-15d0-4f59-a265-5d701fae059d"
+                    title="zartek careers"
+                    frameBorder="0"
+                    height="100%"
+                    width="100%"
+                    style={{ minHeight: "100vh" }}
+                  ></iframe>
                 </div>
               </div>
             </Fragment>
