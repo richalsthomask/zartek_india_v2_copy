7f32acbde4b82ff172fdcc8167c5df5cfcc24fd7
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-10-06 20:49:08 +0530

    fix: intersection observer pathc

--- src/components/Helpers/CountUp.tsx
+++ src/components/Helpers/CountUp.tsx
@@ -1,15 +1,52 @@
-import { useIntersectionObserver } from "@/utils/hooks/useIntersectionObserver";
 import React, { FC, useEffect, useRef, useState } from "react";
 
 export const CountUp: FC<{ count: number }> = ({ count }) => {
   const [currentCounted, setCurrentCounted] = useState<number>(0);
   const [start, setStart] = useState<boolean>(false);
 
+  // function checkIsAndroid() {
+  //   if (typeof navigator === "undefined") {
+  //     return false;
+  //   }
+
+  //   const ua = navigator.userAgent.toLowerCase();
+  //   const isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+  //   if (isAndroid) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
   const ref = useRef();
 
-  useIntersectionObserver(ref, () => {
-    setStart(true);
-  });
+  useEffect(() => {
+    let isCancelled = false;
+
+    if (!isCancelled && typeof IntersectionObserver === "undefined") {
+      return setStart(true);
+    }
+
+    if (!isCancelled && typeof IntersectionObserver !== "undefined") {
+      const observer = new IntersectionObserver((entry) => {
+        entry.forEach((each) => {
+          if (each.isIntersecting) {
+            setStart(true);
+            if (ref.current) {
+              observer?.unobserve(ref.current);
+            }
+          }
+        });
+      });
+      if (ref.current) {
+        observer.observe(ref.current);
+      }
+    }
+
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
 
   useEffect(() => {
     let isCancelled = false;
