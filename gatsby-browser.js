0b1ca63b4471c0ac860044c0a386e5398e27536d
praveenNagaraj97 <praveenacchusn123@gmail.com>
2021-11-02 22:24:31 +0530

    style: slick style sheet added

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,3 @@ import "@/assets/css/fontawesome.css";
 import "@/assets/css/magnific-popup.css";
 import "@/assets/css/style.css";
 import "@/assets/custom.css";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
--- gatsby-config.js
+++ gatsby-config.js
@@ -122,7 +122,7 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: false, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
+        develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         ignore: ["style.css"], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
