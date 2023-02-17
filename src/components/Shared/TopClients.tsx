756ab53fd44d7014b75d286f55ddaee3f130b017
Praveen <praveennagaraj597@gmail.com>
2023-02-18 00:47:47 +0530

    remove icon padding

--- src/components/Shared/TopClients.tsx
+++ src/components/Shared/TopClients.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const TopClients: React.FC = () => {
   return (
@@ -7,32 +7,32 @@ const TopClients: React.FC = () => {
         <h5>Our Amazing Clients</h5>
         <div className="row row-cols-3 row-cols-lg-6 g-2 g-lg-3">
           <div className="col">
-            <div className="p-3 our-clients-logo">
+            <div className="our-clients-logo">
               <img src="/img/cred-logo.svg" className="img-fluid" />
             </div>
           </div>
           <div className="col">
-            <div className="p-3 our-clients-logo">
+            <div className="our-clients-logo">
               <img src="/img/hyundai-logo.svg" className="img-fluid" />
             </div>
           </div>
           <div className="col">
-            <div className="p-3 our-clients-logo">
+            <div className="our-clients-logo">
               <img src="/img/sainaplay-logo.png" className="img-fluid" />
             </div>
           </div>
           <div className="col">
-            <div className="p-3 our-clients-logo">
+            <div className="our-clients-logo">
               <img src="/img/josco-logo.svg" className="img-fluid" />
             </div>
           </div>
           <div className="col">
-            <div className="p-3 our-clients-logo">
+            <div className="our-clients-logo">
               <img src="/img/perch-logo.png" className="img-fluid" />
             </div>
           </div>
           <div className="col">
-            <div className="p-3 our-clients-logo">
+            <div className="our-clients-logo">
               <img src="/img/infosys-logo.svg" className="img-fluid" />
             </div>
           </div>
