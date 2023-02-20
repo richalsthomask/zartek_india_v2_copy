f57c652333e73bbcac315eb47e42f8538790886e
Praveen Nagaraj <praveennagaraj@Praveens-MacBook-Pro.local>
2023-02-20 12:08:15 +0530

    style: change icons color to standard

--- src/templates/HireDedicatedDeveloperDetail/advantages.tsx
+++ src/templates/HireDedicatedDeveloperDetail/advantages.tsx
@@ -1,4 +1,3 @@
-import color from "randomcolor";
 import React from "react";
 
 interface AdvantagesProps {
@@ -20,11 +19,8 @@ const Advantages: React.FC<AdvantagesProps> = ({ data }) => {
         return (
           <div key={each.id} className="col-lg-4 col-sm-6 col-12 mb-4">
             <div className="p-4 dedicated_developers-box rounded-2">
-              <div
-                className="flutterservices-icon-1"
-                style={{ backgroundColor: color({ luminosity: "dark" }), padding: "22px" }}
-              >
-                <img src={each.icon.file.url} className="img-fluid" />
+              <div className="flutterservices-icon-1" style={{ padding: "22px" }}>
+                <img src={each.icon.file.url} alt="..." className="img-fluid" />
               </div>
               <h5 className="py-3 mb-0 text-center mt-3">{each.title}</h5>
             </div>
