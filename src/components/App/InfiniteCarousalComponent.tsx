d60befa78be18f135dcab1d2288e138fbed08404
richalsthomask <richalsthomask@gmail.com>
2024-02-29 16:17:50 +0530

    fix(partner carousal): bug fix to correct padding in partner carousal

--- src/components/App/InfiniteCarousalComponent.tsx
+++ src/components/App/InfiniteCarousalComponent.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { partners } from "@/data/partners";
-import InfiniteCarousal from "../InfiniteCarousal";
 
 export default function InfiniteCarousalComponent({
   className,
@@ -8,21 +7,101 @@ export default function InfiniteCarousalComponent({
   className?: string;
 }): JSX.Element {
   return (
-    <div className="section pbottom-70">
-      <div className=" row">
-        <div className="col-lg-12">
-          <div className="center-heading">
-            <h3 className="section-title">Partners</h3>
-          </div>
-        </div>
+    <div className="section">
+      <div className="center-heading">
+        <h3 className="section-title">Partners</h3>
       </div>
-      <div
-        style={{ maxWidth: "1300px", gap: "30px" }}
-        className="px-sm-4 px-lg-5 d-flex flex-column mx-auto"
-      >
+      <div style={{ gap: "30px" }} className="px-sm-4 px-lg-5 d-flex flex-column mx-auto">
         <InfiniteCarousal items={partners.slice(0, 5)} className={className} />
         <InfiniteCarousal items={partners.slice(5, 11)} className={className} direction={"RIGHT"} />
       </div>
     </div>
+  );
+}
+
+interface InfiniteCarousalProps {
+  items: { label: string; image: string }[];
+  className?: string;
+  direction?: "LEFT" | "RIGHT";
+}
+
+function InfiniteCarousal({
+  items,
+  className,
+  direction = "LEFT",
+}: InfiniteCarousalProps): JSX.Element {
+  const [scrollX, setScrollX] = useState(0);
+  const [resetAnimation, setResetAnimation] = useState(false);
+  const [width, setWidth] = useState(window?.innerWidth);
+  const cardWidth = width > 768 ? 300 : 200;
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setScrollX((scrollX) => scrollX + (direction === "LEFT" ? -10 : 10));
+    }, 150);
+    return () => clearInterval(interval);
+  }, [direction]);
+
+  useEffect(() => {
+    if (
+      direction === "LEFT"
+        ? scrollX <= -cardWidth * items.length - (30 * items.length - 1)
+        : scrollX >= cardWidth * items.length + (30 * items.length - 1)
+    ) {
+      setResetAnimation(true);
+    }
+  }, [scrollX, items, direction, cardWidth]);
+
+  useEffect(() => {
+    if (resetAnimation) {
+      setScrollX(direction === "LEFT" ? 5 : -5);
+      setTimeout(() => {
+        setResetAnimation(false);
+      }, 2);
+    }
+  }, [resetAnimation, direction]);
+  return (
+    <section
+      style={{ gap: "30px", overflow: "hidden" }}
+      className={
+        "py-3 w-100 mx-auto d-flex flex-row align-items-center " +
+        (direction === "LEFT" ? "justify-content-start" : "justify-content-end") +
+        " " +
+        className
+      }
+    >
+      {[...items, ...items].map((item, index) => {
+        return (
+          <div
+            style={{
+              transform: `translateX(${scrollX}px)`,
+              transitionDuration: resetAnimation ? "0ms" : "300ms",
+              // gap: "5px",
+              minWidth: cardWidth + "px",
+              height: "120px",
+              border: "2px solid #f4f8fb",
+              borderRadius: "20px",
+              boxShadow: "0 6px 12px 1px #f4f8fb",
+            }}
+            className={`body bg-white d-flex flex-column align-items-center justify-content-center p-3`}
+            key={index}
+          >
+            <img src={item.image} alt={item.label} style={{ width: "140px" }} />
+            {/* {item.label} */}
+          </div>
+        );
+      })}
+    </section>
   );
 }
